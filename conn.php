baa073b3abb2d5e66401e600a23156707a431d47
Maria Lindén <marialinden2@hotmail.com>
2012-10-25 12:06:32 +0200

    nya filer

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+//connection
+
+$db_hostname = 'localhost';
+$db_database = 'blogg';
+$db_username = 'root';
+$db_password = '';
+
+?>
---
+++ show_image.php
@@ -0,0 +1,25 @@
+<?php
+//header("Content-type: image/jpeg");
+	
+	$ID = $_GET['imageID'];
+ 
+	$dbConn = mysqli_connect("localhost", "root", "", "blogg");	
+	$sql = "SELECT imageName FROM gallery WHERE imageID=$ID";
+	$res = mysqli_query($dbConn, $sql);
+	$row = mysqli_fetch_assoc($res);
+	
+	$bigImage = $row['imageName'];
+	echo "<div class='box'>";
+	echo "<img src='images/$bigImage'>";
+	echo "</div>";
+?>
+<!DOCTYPE>
+<html>
+<head>
+<title>Stora bilder</title>
+<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8"/>
+<link href='style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+</body>
+</html>
